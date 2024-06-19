26519479094dbd5dc4ee1eeca5e4e19ef01eec8d
metafosfato <arkuzumaki@gmail.com>
2024-06-19 17:50:11 -0300

    questao

---
+++ Questao3/Lista.java
@@ -0,0 +1,16 @@
+public interface Lista {
+    public void insereInicio(int valor);
+
+    public void insereFim(int valor);
+
+    public boolean estahVazia();
+
+    public void imprimeLista();
+
+    public boolean remove();
+
+    public int tamanho();
+
+    public int nroPares();
+
+}
--- ex7/Noh.java
+++
@@ -1,9 +0,0 @@
-public class Noh {
-    public int valor;
-    public Noh proximo;
-
-    public Noh(int valor) {
-        this.valor = valor;
-        this.proximo = null;
-    }
-}
